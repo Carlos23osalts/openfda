7ce2d836a3460a35db528fadba52f184127a701a
c.osuna.2017 <carlososunaaltes@gmail.com>
2018-05-13 20:22:54 +0200

    finished!

--- openfda-project/server.py
+++ openfda-project/server.py
@@ -119,7 +119,7 @@ class  testHTTPHandler(http.server.BaseHTTPRequestHandler):
         except KeyError:
             self.send_response(404)
             list2.append("Error 404: Webpage not found")
-            OpenFDAHTML.texto(self, list2)
+            OpenFDAHTML.texto(self, list)
         return
 
 Handler = http.server.SimpleHTTPRequestHandler
